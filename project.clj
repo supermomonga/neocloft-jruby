0c81164c30321e7d890b5c6c6db9ad2af17123ce
supermomonga <hi@supermomonga.com>
2013-11-10 23:31:59 +0900

    remove clojure dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
   :dependencies [
-                 [org.clojure/clojure "1.5.1"]
                  [neocloft/neocloft "1.0.0-SNAPSHOT"]
                  [org.jruby/jruby-complete "1.7.6"]]
   :aot :all)
